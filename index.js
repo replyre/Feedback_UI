167a0859c56f31d364b95f3506f41f235718358e
replyre <121796450+replyre@users.noreply.github.com>
2023-09-02 04:47:54 +0530

    formsubmit features + feedback text addede

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const iconbtn = document.querySelectorAll(".icon");
 const btn = document.getElementById("btn");
+const container = document.getElementById("container");
+document.getElementById("btn").disabled = true;
 let selectedRating = "";
 iconbtn.forEach((icon) => {
   icon.addEventListener("click", (e) => {
@@ -17,9 +19,12 @@ iconbtn.forEach((icon) => {
 
     // console.log(e.target.innerText || e.target.parentNode.innerText);
     selectedRating = e.target.parentNode.innerText.split("\n")[0];
+    document.getElementById("btn_back").style.display = "block";
+    document.getElementById("btn").disabled = false;
   });
 });
-btn.addEventListener("click", () => {
+
+function feedback() {
   if (selectedRating == "") {
     document.querySelector(".error").style.display = "block";
 
@@ -28,7 +33,24 @@ btn.addEventListener("click", () => {
     }, 2000);
     return;
   }
-});
+  var flag = true;
+  document.querySelectorAll(".text").forEach((field) => {
+    if (field.value.length === 0) flag = false;
+  });
+
+  console.log(flag);
+  if (flag == false) return;
+
+  setTimeout(() => {
+    container.innerHTML = `<strong>Thank You !</strong>
+   <strong>Feedback: ${selectedRating}</strong>
+   <p>We'll use your feedback to improve our customer support.</p>.
+   <div class="button" onclick="location.reload()"><p>Home</p></div>
+   `;
+    container.style.cssText =
+      " border :2px solid rgb(44, 143, 236);  box-shadow: 0px 0px 12px 2px  rgba(44, 143, 236,0.5);";
+  }, 1000);
+}
 
 function removeActive() {
   iconbtn.forEach((icon) => {
